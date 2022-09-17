07da910d8f5e73cf2265b444349168cba3ad0c53
su <rakdav@inbox.ru>
2022-09-17 12:46:58 +0200

    ViewModel add

---
+++ TaskManager/Model/InfoProcessClass.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Model
+{
+    internal class InfoProcessClass: INotifyPropertyChanged
+    {
+        private int idProcess;
+        private DateTime timeStartPro;
+        private int cpuTime;
+        private int countThread;
+        private int countProcess;
+        public int IdProcess
+        {
+            get { return idProcess; }
+            set
+            {
+                idProcess = value;
+                OnPropertyChanged("idProcess");
+            }
+        }
+        public DateTime TimeStartPro
+        {
+            get { return timeStartPro; }
+            set
+            {
+                timeStartPro = value;
+                OnPropertyChanged("idProcess");
+            }
+        }
+        public int CPUTime
+        {
+            get { return cpuTime; }
+            set
+            {
+                cpuTime = value;
+                OnPropertyChanged("idProcess");
+            }
+        }
+
+        public int CountThread
+        {
+            get { return countThread; }
+            set
+            {
+                countThread = value;
+                OnPropertyChanged("idProcess");
+            }
+        }
+        public int CountProcess
+        {
+            get { return countProcess; }
+            set
+            {
+                countProcess = value;
+                OnPropertyChanged("idProcess");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ TaskManager/ViewModel/ProcessInfoViewModel .cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Model;
+
+namespace TaskManager.ViewModel
+{
+    internal class ProcessInfoViewModel
+    {
+        private InfoProcessClass selectedProcess;
+        public ObservableCollection<InfoProcessClass> InfoProcesses { get; set; }
+        public InfoProcessClass SelectedProcess
+        {
+            get { return selectedProcess; }
+            set
+            {
+                selectedProcess = value;
+                OnPropertyChanged("SelectedProcess");
+            }
+        }
+
+        public ProcessInfoViewModel()
+        {
+            InfoProcesses = new ObservableCollection<InfoProcessClass>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
